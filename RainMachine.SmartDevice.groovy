cc053282173d616fda4864b14ae42e2a50825213
copy-ninja <jason@copyninja.net>
2014-12-22 01:27:25 -0600

    Update RainMachine.SmartDevice.groovy

--- RainMachine.SmartDevice.groovy
+++ RainMachine.SmartDevice.groovy
@@ -33,12 +33,12 @@ metadata {
 	definition (name: "RainMachine", namespace: "copy-ninja", author: "Jason Mok") {
 		capability "Valve"
 		capability "Refresh"
-        capability "Polling"
-        
-        attribute "runTime", "number"
-        
-        command "stopAll"
-        command "setRunTime"
+	        capability "Polling"
+	        
+	        attribute "runTime", "number"
+	        
+	        command "stopAll"
+	        command "setRunTime"
 	}
 
 	simulator { }
@@ -46,19 +46,19 @@ metadata {
 	tiles {
 		standardTile("contact", "device.contact", width: 2, height: 2, canChangeIcon: true) {
 			state "closed", label: 'inactive', action: "valve.open", icon: "st.Outdoor.outdoor12", backgroundColor: "#ffffff"
-            state "open", label: 'active', action: "valve.close", icon: "st.Outdoor.outdoor12", backgroundColor: "#1e9cbb" 			
-            state "opening", label: 'pending', action: "valve.close", icon: "st.Outdoor.outdoor12", backgroundColor: "#D4741A" 	
+	            	state "open", label: 'active', action: "valve.close", icon: "st.Outdoor.outdoor12", backgroundColor: "#1e9cbb" 			
+	            	state "opening", label: 'pending', action: "valve.close", icon: "st.Outdoor.outdoor12", backgroundColor: "#D4741A" 	
 		}
 		standardTile("refresh", "device.switch", inactiveLabel: false, decoration: "flat") {
 			state "default", label:'', action:"refresh.refresh", icon:"st.secondary.refresh"
 		}
-        standardTile("stopAll", "device.switch", inactiveLabel: false, decoration: "flat") {
+        	standardTile("stopAll", "device.switch", inactiveLabel: false, decoration: "flat") {
 			state "default", label:'Stop All', action:"stopAll", icon:"st.secondary.off"
 		}
-        controlTile("runTimeControl", "device.runTime", "slider", height: 1, width: 2, inactiveLabel: false) {
+        	controlTile("runTimeControl", "device.runTime", "slider", height: 1, width: 2, inactiveLabel: false) {
 			state "setRunTime", action:"setRunTime", backgroundColor: "#1e9cbb"
-        }
-        valueTile("runTime", "device.runTime", inactiveLabel: false, decoration: "flat") {
+        	}
+        	valueTile("runTime", "device.runTime", inactiveLabel: false, decoration: "flat") {
 			state "runTimeValue", label:'${currentValue} mins', backgroundColor:"#ffffff"
 		}
 
@@ -67,46 +67,44 @@ metadata {
 	}
 }
 
-def installed() {
-	runTime = 5
-}
+def installed() { runTime = 5 }
 
 def parse(String description) {}
 
 def open()  { 
 	def runTime = device.currentValue("runTime")
 	def runTimeInSecs = runTime * 60
-    parent.sendCommand(this, "start", runTimeInSecs, "") 
-    poll()  
+    	parent.sendCommand(this, "start", runTimeInSecs, "") 
+    	poll()  
 }
 def close() { 
 	def runTime = device.currentValue("runTime")
 	def runTimeInSecs = runTime * 60
-    parent.sendCommand(this, "stop",  runTimeInSecs, "") 
-    poll()  
+    	parent.sendCommand(this, "stop",  runTimeInSecs, "") 
+    	poll()  
 }
 
 def refresh() {
-    parent.refresh()
-    poll()
+    	parent.refresh()
+	poll()
 }
 
 def poll() {
-    def deviceStatus = parent.getDeviceStatus(this)
-    if (deviceStatus == "inactive") {
-        sendEvent(name: "contact", value: "closed", display: true, descriptionText: device.displayName + " was inactive")
-    }
-    if (deviceStatus == "active") {
-        sendEvent(name: "contact", value: "open", display: true, descriptionText: device.displayName + " was active")
-    }   
-    if (deviceStatus == "pending") {
-        sendEvent(name: "contact", value: "opening", display: true, descriptionText: device.displayName + " was pending")
-    }  
+	def deviceStatus = parent.getDeviceStatus(this)
+	if (deviceStatus == "inactive") {
+		sendEvent(name: "contact", value: "closed", display: true, descriptionText: device.displayName + " was inactive")
+	}
+	if (deviceStatus == "active") {
+		sendEvent(name: "contact", value: "open", display: true, descriptionText: device.displayName + " was active")
+	}   
+	if (deviceStatus == "pending") {
+		sendEvent(name: "contact", value: "opening", display: true, descriptionText: device.displayName + " was pending")
+	}  
 }
 
 def stopAll() {
 	parent.sendStopAll()
-    poll()
+	poll()
 }
 
 void setRunTime(runTimeSecs) {
